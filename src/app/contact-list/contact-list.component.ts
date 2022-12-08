79e67e3812264ff513fc1f765537a72edc5f5403
Lara Ayrolla <lara@pop-os.localdomain>
2022-12-07 23:08:58 -0300

    adding exact filters

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -10,6 +10,7 @@ import { DOCUMENT } from '@angular/common';
 })
 export class ContactListComponent {
   contacts: any;
+  url: any;
   constructor(@Inject(DOCUMENT) document: Document, private http: HttpClient) {}
   ngOnInit(): void {
     this.http.get('http://localhost:3000/contacts').subscribe(res => {
@@ -24,6 +25,24 @@ export class ContactListComponent {
     const url = 'http://localhost:3000/contacts?name_like=' + name + '&phone_like=' + phone
       + '&email_like=' + email + '&company_like=' + company;
     this.http.get(url).subscribe(res => {
+      this.contacts = res
+    })
+  }
+  filterContactsExact(){
+    const name = (<HTMLInputElement>document.getElementById("name")).value;
+    const phone = (<HTMLInputElement>document.getElementById("phone")).value
+    const email = (<HTMLInputElement>document.getElementById("email")).value
+    const company = (<HTMLInputElement>document.getElementById("company")).value
+    this.url = 'http://localhost:3000/contacts';
+    if (name!=null && name!='')
+      this.url+= '?name=' + name;
+    if (phone!=null && phone!='')
+      this.url+= '&phone=' + phone;
+    if (email!=null && email!='')
+      this.url+= '&email=' + email;
+    if (company!=null && company!='')
+      this.url+= '&company=' + company;
+    this.http.get(this.url).subscribe(res => {
       this.contacts = res
     })
   }
